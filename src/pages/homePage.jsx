72ee2b733fa098be3bb2b00b9fcd2ec5b926bfe3
malki nuwanthika <nuwanthikawee19@gmail.com>
2024-12-19 13:30:54 +0530

    implementing routes and useState

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './homePage.css';
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <header>
+        <h1>Welcome to Our Website</h1>
+        <p>Your one-stop destination for all things awesome.</p>
+      </header>
+      <main>
+        <button>Learn More</button>
+        <button>Contact Us</button>
+      </main>
+      <Link to="/login">Login</Link> 
+    </div> 
+
+  );
+}
+
+
